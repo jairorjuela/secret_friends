98e9f40f3925da012903b255c7900f72e810c635
jairorjuela <genius85862@gmail.com>
2022-09-11 18:39:20 -0500

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+locations = Location.create([
+  { name: 'Marketing' },
+  { name: 'Tech' },
+  { name: 'Soporte' }
+])
+
+workers = Worker.create([
+  { name: "Eddard Stark", location: Location.find_by(name: 'Marketing' },
+  { name: "Robert Baratheon" location: 'Tech' },
+  { name: "Jaime Lannister" location: 'Soporte' },
+  { name: "Catelyn Stark" location: 'Soporte' },
+  { name: "Cersei Lannister" location: 'Marketing' },
+  { name: "Daenerys Targaryen" location: 'Tech' },
+  { name: "Jorah Mormont" location: 'Marketing' },
+  { name: "Viserys Targaryen" location: 'Marketing' },
+  { name: "Jon Snow" location: 'Soporte' },
+  { name: "Robb Stark" location: 'Tech' },
+  { name: "Sansa Stark" location: 'Soporte' },
+  { name: "Arya Stark" location: 'Marketing' },
+  { name: "Theon Greyjoy" location: 'Soporte' },
+  { name: "Joffrey Baratheon" location: 'Tech' },
+  { name: "Sandor Clegane" location: 'Soporte' },
+  { name: "Tyrion Lannister" location: 'Marketing' },
+  { name: "Davos Seaworth" location: 'Tech' },
+  { name: "Samwell Tarly" location: 'Tech' },
+  { name: "Stannis Baratheon" location: 'Soporte' },
+])
