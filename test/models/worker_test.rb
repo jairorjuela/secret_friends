f1c7d4891b8ff27a9fe606b947818c677bbb1944
Alexis Franco <alexisfrancolopez@gmail.com>
2022-09-18 12:35:16 -0500

    Test examples to Worker

--- test/models/worker_test.rb
+++ test/models/worker_test.rb
@@ -1,7 +1,60 @@
 require "test_helper"
 
 class WorkerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @location = locations(:location_one)
+    @worker = workers(:worker_one)
+  end
+
+  # DB columns
+  test 'name column' do
+    assert Worker.column_names.include?('name'), 'name column exist in Worker table'
+    assert_equal 'string', worker_type_column('name').type.to_s, 'correctly name type'
+    assert_equal false, worker_type_column('name').null, 'require name value'
+  end
+
+  # Associations
+  test 'belongs_to :location relation' do
+    assert_equal @location, @worker.location, 'relation between worker and location'
+  end
+
+  test 'has_many :games relation' do
+    assert_equal 1, @worker.games.size, 'relation between worker and game'
+  end
+
+  # Index
+  test 'name indexes' do
+    indexes = ::ActiveRecord::Base.connection.indexes(Worker.table_name)
+    matched_index = indexes.detect { |each| each.columns == ['location_id'] }
+
+    assert_equal true, matched_index.present?, 'location_id index exist in worker table'
+  end
+
+  # Validations
+  test 'invalid without name' do
+    @worker.name = nil
+
+    refute @worker.valid?
+    assert @worker.errors[:name].present?, 'error without name'
+  end
+
+  test 'invalid name length' do
+    @worker.name = 'Ary'
+
+    refute @worker.valid?
+    assert @worker.errors[:name].present?, 'error with too short length name'
+
+    @worker.name = 'A'*50
+    refute @worker.valid?
+    assert @worker.errors[:name].present?, 'error with too long length name'
+  end
+
+  # Methods
+  test 'couple_games method' do
+    assert_equal 1, @worker.couple_games.size, 'quantity games'
+  end
+
+  def worker_type_column(column)
+    Worker.columns.detect { |each| each.name == column.to_s }
+  end
 end
---
+++ test/support/controller/workers_support.rb
@@ -0,0 +1,23 @@
+module WorkersSupport
+  def worker_params
+    {
+      worker: {
+        name: 'New Worker',
+        location_id: locations(:location_one).id
+      }
+    }
+  end
+
+  def invalid_worker_params
+    {
+      worker: {
+        name: 'Inv',
+        location_id: locations(:location_one).id
+      }
+    }
+  end
+
+  def create_worker_valid_keys
+    %w[id name location year_in_work]
+  end
+end
